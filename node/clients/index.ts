a7ddf654ed89c5d4f523268f2cac802fda2450f0
Daniel Arango <arangomonsalve.daniel@gmail.com>
2023-09-15 11:23:08 -0500

    Order Broadcast

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,4 +1,11 @@
 import { IOClients } from "@vtex/api";
+import { SqsClient } from "./events";
 
 // Extend the default IOClients implementation with our own custom clients.
-export class Clients extends IOClients {}
+export class Clients extends IOClients {
+
+  public get sendEvents() {
+    return this.getOrSet('sqsClient', SqsClient)
+  }
+
+}
--- node/middlewares/allStates.ts
+++ node/middlewares/allStates.ts
@@ -2,6 +2,12 @@ export async function allStates(
   ctx: StatusChangeContext,
   next: () => Promise<any>
 ) {
+   const {
+     clients: { sendEvents }
+   } = ctx
+
   console.log(ctx.body)
+   await sendEvents.sendMessage('XXXXXXXXX', JSON.stringify(ctx.body))
+  
   await next()
 }
